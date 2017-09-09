50ae158de92578162161fbfabe92ed4de67b0928
admarkov <admarkov@lenta.ru>
2017-09-09 22:18:21 +0300

    Implemented polygons for every item

--- diagramitem.cpp
+++ diagramitem.cpp
@@ -8,23 +8,26 @@ DiagramItem::DiagramItem(DiagramType type, QGraphicsItem *parent)
     switch(diagramType()) {
         case StartEnd: {
             QPainterPath path;
-            path.moveTo(200, 50);
-            path.arcTo(150, 0, 50, 50, 0, 90);
-            path.arcTo(50, 0, 50, 50, 90, 90);
-            path.arcTo(50, 50, 50, 50, 180, 90);
-            path.arcTo(150, 50, 50, 50, 270, 90);
-            path.lineTo(200, 25);
+            path.moveTo(-60, -40);
+            path.lineTo(40, -40);
+            path.arcTo(40,-40,40,40,90,-90);
+            path.lineTo(80, 20);
+            path.arcTo(40,0,40,40,0,-90);
+            path.lineTo(-60,40);
+            path.arcTo(-80,0,40,40,-90,-90);
+            path.lineTo(-80,-20);
+            path.arcTo(-80,-40,40,40,-180,-90);
             _polygon = path.toFillPolygon();
             break;
     }
         case Conditional:
-            _polygon << QPointF(-70, 0) << QPointF(0, 40) << QPointF(70, 0) << QPointF(0, -40) << QPointF(-70, 0);
+            _polygon << QPointF(0, 40) << QPointF(80, 0) << QPointF(0, -40) << QPointF(-80, 0) << QPointF(0, 40);
             break;
         case Step:
-            _polygon << QPointF(-100, -100) << QPointF(100, -100) << QPointF(100, 100) << QPointF(-100, 100) << QPointF(-100, -100);
+            _polygon << QPointF(-80, 40) << QPointF(80, 40) << QPointF(80, -40) << QPointF(-80, -40) << QPointF(-80, 40);
              break;
         default:
-            _polygon << QPointF(-120, -80) << QPointF(-70, 80) << QPointF(120, 80) << QPointF(70, -80) << QPointF(-120, -80);
+            _polygon << QPointF(-80, 40) << QPointF(40, 40) << QPointF(80, -40) << QPointF(-40, -40) << QPointF(-80, 40);
             break;
     }
 
